948c191d725806a86a7cd7c0ec0c27cee5634c67
Eric <ericxyliu@kkcompany.com>
2023-11-18 21:36:01 +0800

    fix: enhance the sync data log message

--- sync_price.py
+++ sync_price.py
@@ -62,25 +62,28 @@ def send_notify(message):
 uniqlo_model = UniqloModel()
 products = uniqlo_model.get_product_list()
 
-try:
-    send_notify('\n開始更新')
-    time_start = time.time()
-    for product in products:
+send_notify('\n開始更新')
+time_start = time.time()
+for product in products:
+    try:
+        step = 'Begin'
         product_id = product['product_id']
         print(f"==== {product_id} ====")
         print("--- GET SITE DATA ---")
+        step = 'Get Site Data'
         product_data_website = get_official_site_data(product_id)
         if product_data_website:
             print("--- GET DB DATA ---")
+            step = 'Get DB Data'
             price = int(float(product_data_website['prices'][0]))
             min_price_db = product['min_price']
             min_price = min_price_db if min_price_db < price else price
             print("--- UPDATE DB ---")
+            step = 'Update DB'
             uniqlo_model.daily_update(product_id=product_id, price=price, min_price=min_price)
+    except Exception as e:
+        send_notify(f"\n更新發生錯誤\nStep:{step}\nErrorMsg:{str(e)}\nproduct_id:{product_id}")
+time_end = time.time()
 
-    time_end = time.time()
-except Exception as e:
-    send_notify(f"\n更新發生錯誤\nErrorMsg:{str(e)}\nproduct_id:{product_id}")
-    exit()
 time_c= time_end - time_start
 send_notify(f"\n同步成功！\n========\n共花費: {round(time_c)} sec\n更新:{len(products)}筆資料")
